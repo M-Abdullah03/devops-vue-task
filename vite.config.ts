0425c5f31343dc32b4224bc517df7646be613574
M-Abdullah03 <m18ab03@gmail.com>
2025-02-14 17:08:02 +0500

    Workflow updateddd

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(({  mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
-      DEV_PORT: env.VITE_APP_DEV_PORT,
+      DEV_PORT: JSON.stringify(env.VITE_APP_DEV_PORT), // Make it valid JSON
     },
     server: {
       port: Number(env.VITE_APP_DEV_PORT) || 3000,
